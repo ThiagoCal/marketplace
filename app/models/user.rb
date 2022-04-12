922ad465d058246bb3e6bb735815a1de9357a5cd
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-04-12 00:20:00 -0300

    setting up a marketplace starter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :email, presence: true
+  validates :address, presence: true
+  validates :postcode, presence: true
+  has_many :sales, dependent: :destroy
+  has_many :products, dependent: :destroy
 end
