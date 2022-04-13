591ef18b2afc7cacadca1f4c6cd19c99510dcf85
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-04-13 18:41:37 -0300

    displaying products from a json file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
-  belongs_to :category
+  # belongs_to :category
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
+  # validates :name, presence: true
+  # validates :description, presence: true
+  # validates :price, presence: true
 
-  scope :category_id, ->(category_id) { where category_id: category_id }
+  # scope :category_id, ->(category_id) { where category_id: category_id }
 end
